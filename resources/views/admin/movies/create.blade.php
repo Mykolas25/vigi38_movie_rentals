a72171c017e05fa076f7b5c6a9f361c56f5654e9
Mykolas25 <justmikis@gmail.com>
2023-03-22 16:07:55 +0200

    create view image component

--- resources/views/admin/movies/create.blade.php
+++ resources/views/admin/movies/create.blade.php
@@ -41,15 +41,8 @@
             </div>
 
             <div class="form-group">
-                <label for="exampleInputFile">File input</label>
-                <div class="input-group">
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                    </div>
-                    <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                    </div>
+                <div class="form-group">
+                    <x-forms.image-input :images="$movie->images" />
                 </div>
             </div>
         </div>
