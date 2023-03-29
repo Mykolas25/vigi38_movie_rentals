e78981222b7fb0a1365b96f34d713c4b06f5f988
Mykolas25 <justmikis@gmail.com>
2023-03-29 22:15:28 +0300

    Update sidebar.blade.php
    translate/ add logout

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -32,15 +32,12 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-
                 <li class="nav-header">Duomenų lentelės</li>
                 <li class="nav-item">
                     <a href="{{ route('admin.movies.index') }}" class="nav-link">
                         <i class="nav-icon far fa-calendar-alt"></i>
                         <p>
-                            Filmai
+                            @langTitle('app.movies')
                         </p>
                     </a>
                 </li>
@@ -48,13 +45,19 @@
                     <a href="{{ route('admin.actors.index') }}" class="nav-link">
                         <i class="nav-icon far fa-calendar-alt"></i>
                         <p>
-                            Aktoriai
+                            @langTitle('app.actors')
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.logout') }}" class="nav-link">
+                        <i class="nav-icon far fa-calendar-alt"></i>
+                        <p>
+                            @langTitle('app.logout')
                         </p>
                     </a>
                 </li>
             </ul>
         </nav>
-        <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
 </aside>
