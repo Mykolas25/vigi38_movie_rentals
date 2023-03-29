51e510662fb761e12499736ad8ffffa506feed3d
Mykolas25 <justmikis@gmail.com>
2023-03-29 12:44:23 +0300

    update actors index view/ rename var $actors to var $models, translate static strings

--- resources/views/admin/actors/index.blade.php
+++ resources/views/admin/actors/index.blade.php
@@ -1,73 +1,74 @@
 @extends('admin.layouts.document')
 
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Aktoriai</h3>
-                <a href="{{ route('admin.actors.create') }} " class="btn btn-app">
-                  <i class="fas fa-plus"></i> Naujas
-                </a>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Vardas</th>
-                            <th>Pavardė</th>
-                            <th>Gimimo data</th>
-                            <th>Sukurta</th>
-                            <th>Atnaujinta</th>
-                            <th>Veiksmai</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($actors as $actor)
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ Str::ucfirst(trans('app.actors')) }}</h3>
+                    <a href="{{ route('admin.actors.create') }} " class="btn btn-app">
+                        <i class="fas fa-plus"></i> {{ Str::ucfirst(trans('app.new')) }}
+                    </a>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="admin-table" class="table table-bordered table-striped">
+                        <thead>
                             <tr>
-                                <td>{{ ($actor->id ?? '')}} </td>
-                                {{-- <td>
-                                    <img width="100" src="{{ ($actor->image ?? '')}} ">
-                                </td> --}}
-                                <td> {{ ($actor->first_name ?? '') }} </td>
-                                <td> {{ ($actor->last_name ?? '') }} </td>
-                                <td> {{ ($actor->date_of_birth ?? '') }} </td>
-                                <td> {{ ($actor->created_at ?? '') }} </td>
-                                <td> {{ ($actor->updated_at ?? '') }} </td>
-                                <td>
-                                    <div class="btn-group">
-                                        <a href='{{ route('admin.actors.edit', $actor) }} ' type="button" class="btn btn-info">Keisti</a>
-                                        <button type="button" class="btn btn-info dropdown-toggle dropdown-icon" data-toggle="dropdown" aria-expanded="false">
-                                            <span class="sr-only">Toggle Dropdown</span>
-                                        </button>
-                                        <div class="dropdown-menu" role="menu">
-                                            <a class="dropdown-item" href="{{ route('admin.actors.destroy', $actor) }} ">Naikinti</a>
-                                        </div>
-                                    </div>
-                                </td>
+                                <th>ID</th>
+                                <th>{{ Str::ucfirst(trans('app.first_Name')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.last_name')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.date_of_birth')) }} </th>
+                                <th>{{ Str::ucfirst(trans('app.created_at')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.updated_at')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.actions')) }}</th>
                             </tr>
-                       @endforeach
-                    </tbody>
-                    <tfoot>
-                        <tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($models as $model)
+                                <tr>
+                                    <td>{{ $model->id ?? '' }} </td>
+                                    {{-- <td>
+                                    <img width="100" src="{{ ($model->image ?? '')}} ">
+                                </td> --}}
+                                    <td> {{ $model->first_name ?? '' }} </td>
+                                    <td> {{ $model->last_name ?? '' }} </td>
+                                    <td> {{ $model->date_of_birth ?? '' }} </td>
+                                    <td> {{ $model->created_at ?? '' }} </td>
+                                    <td> {{ $model->updated_at ?? '' }} </td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <a href='{{ route('admin.actors.edit', $model) }} ' type="button"
+                                                class="btn btn-info">Keisti</a>
+                                            <button type="button" class="btn btn-info dropdown-toggle dropdown-icon"
+                                                data-toggle="dropdown" aria-expanded="false">
+                                                <span class="sr-only">Toggle Dropdown</span>
+                                            </button>
+                                            <div class="dropdown-menu" role="menu">
+                                                <a onclick="event.preventDefault()" class="dropdown-item delete"
+                                                    href="{{ route('admin.actors.destroy', $model) }} ">Naikinti</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
                             <th>ID</th>
-                            <th>Vardas</th>
-                            <th>Pavardė</th>
-                            <th>Gimimo data</th>
-                            <th>Sukurta</th>
-                            <th>Atnaujinta</th>
-                            <th>Veiksmai</th>
-                        </tr>
-                    </tfoot>
-                </table>
+                                <th>{{ Str::ucfirst(trans('app.first_Name')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.last_name')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.date_of_birth')) }} </th>
+                                <th>{{ Str::ucfirst(trans('app.created_at')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.updated_at')) }}</th>
+                                <th>{{ Str::ucfirst(trans('app.actions')) }}</th>
+                        </tfoot>
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
-            <!-- /.card-body -->
+            <!-- /.card -->
         </div>
-        <!-- /.card -->
+        <!-- /.col -->
     </div>
-    <!-- /.col -->
-</div>
-<!-- /.row -->
+    <!-- /.row -->
 @endsection
