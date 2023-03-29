1f6aa1ffcd141c316a5335adc2d8ef02ef7512d7
Mykolas25 <justmikis@gmail.com>
2023-03-29 17:08:44 +0300

    Update FrontMovieController.php
    smallfix

--- app/Http/Controllers/Front/FrontMovieController.php
+++ app/Http/Controllers/Front/FrontMovieController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Front;
 
 use App\Models\Movie;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
 
 class FrontMovieController extends Controller
