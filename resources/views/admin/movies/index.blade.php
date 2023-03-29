d4507d1ecd70d0c091b27205f239d65fe5020b49
Mykolas25 <justmikis@gmail.com>
2023-03-29 17:05:59 +0300

    Update index.blade.php
    use custom translation directives @langTitle
    change var $movie to var $model

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -1,97 +1,99 @@
-@extends('admin.layouts.document')
+@extends('admin.layouts.document', ['title' => Str::title(__('app.movies'))])
 
 @section('content')
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Filmai</h3>
-                <a href="{{ route('admin.movies.create') }} " class="btn btn-app">
-                  <i class="fas fa-plus"></i> Naujas
-                </a>
+                <h3 class="card-title">@langTitle('app.list')</h3>
+                <span class='header-actions'>
+                    <a href="{{ route('admin.movies.create') }} " class="btn btn-app new-resource">
+                        <i class="fas fa-plus"></i> @langTitle('app.new')
+                    </a>
+                </span>
             </div>
-            <!-- /.card-header -->
             <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
+                <table id="admin-table" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>{{Str::ucfirst(trans('app.image'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.title'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.date'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.description'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.runtime'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.rating'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.genres'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.actors'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.created_at'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.updated_at'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.actions'))}}</th>
+                            <th>@langTitle('app.image')</th>
+                            <th>@langTitle('app.title')</th>
+                            <th>@langTitle('app.date')</th>
+                            <th>@langTitle('app.description')</th>
+                            <th>@langTitle('app.runtime')</th>
+                            <th>@langTitle('app.rating')</th>
+                            <th>@langTitle('app.genres')</th>
+                            <th>@langTitle('app.actors')</th>
+                            <th>@langTitle('app.created_at')</th>
+                            <th>@langTitle('app.updated_at')</th>
+                            <th>@langTitle('app.actions')</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($movies as $movie)
+                        @foreach ($models as $model)
                         <tr>
-                                <td>{{ ($movie->id ?? '')}} </td>
-                                <td>
-                                    <img width="100" src="{{ asset('storage/images/'.($movie->image ?? "noimage.jpg"))}} ">
+                            <td>{{ ($model->id ?? '')}} </td>
+                            <td>
+                                <img width="100" src="{{ asset('storage/images/'.($model->image ?? " noimage.jpg"))}} ">
                                 </td>
-                                <td> {{ ($movie->title ?? '') }} </td>
-                                <td> {{ ($movie->release_date ?? '') }} </td>
-                                <td> {{ ($movie->description ?? '') }} </td>
-                                <td> {{ ($movie->runtime ?? '') }} </td>
-                                <td> {{ ($movie->rating ?? '') }} </td>
+                                <td> {{ ($model->title ?? '') }} </td>
+                                <td> {{ ($model->release_date ?? '') }} </td>
+                                <td> {{ ($model->description ?? '') }} </td>
+                                <td> {{ ($model->runtime ?? '') }} </td>
+                                <td> {{ ($model->rating ?? '') }} </td>
                                 <td>
-                                     @foreach($movie->genres as $genre)
+                                     @foreach($model->genres as $genre)
                                         {{$genre->name ?? ''}}
-                                     @endforeach 
+                                     @endforeach
                                 </td>
                                 <td>
-                                     @foreach($movie->actors as $actor)
-                                        <a href="{{route('admin.actors.edit', $actor)}}">
-                                            {{$actor->fullName ?? ''}}
+                                     @foreach($model->actors as $actor)
+                                        <a href=" {{route('admin.actors.edit', $actor)}}">
+                                {{$actor->fullName ?? ''}}
+                                </a>
+                                @endforeach
+                            </td>
+                            <td> {{ ($model->created_at ?? '') }} </td>
+                            <td> {{ ($model->updated_at ?? '') }} </td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{ route('admin.movies.edit', $model) }}" type="button"
+                                        class="btn btn-info">@langTitle('app.edit')</a>
+                                    <button type="button" class="btn btn-info dropdown-toggle dropdown-icon"
+                                        data-toggle="dropdown" aria-expanded="false">
+                                        <span class="sr-only">Toggle Dropdown</span>
+                                    </button>
+                                    <div class="dropdown-menu" role="menu">
+                                        <a onclick="event.preventDefault()" class="dropdown-item delete"
+                                            href="{{ route('admin.movies.destroy', $model) }}">
+                                            @langTitle('app.delete')
                                         </a>
-                                     @endforeach 
-                                </td>
-                                <td> {{ ($movie->created_at ?? '') }} </td>
-                                <td> {{ ($movie->updated_at ?? '') }} </td>
-                                <td>
-                                    <div class="btn-group">
-                                        <a href='{{ route('admin.movies.edit', $movie) }} ' type="button" class="btn btn-info">Keisti</a>
-                                        <button type="button" class="btn btn-info dropdown-toggle dropdown-icon" data-toggle="dropdown" aria-expanded="false">
-                                            <span class="sr-only">Toggle Dropdown</span>
-                                        </button>
-                                        <div class="dropdown-menu" role="menu">
-                                            <a onclick="event.preventDefault()" class="dropdown-item delete" href="{{ route('admin.movies.destroy', $movie) }} ">Naikinti</a>
-                                        </div>
                                     </div>
-                                </td>
-                            </tr>
-                       @endforeach
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
                     </tbody>
                     <tfoot>
                         <tr>
                             <th>ID</th>
-                            <th>{{Str::ucfirst(trans('app.image'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.title'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.date'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.description'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.runtime'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.rating'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.genres'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.actors'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.created_at'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.updated_at'))}}</th>
-                            <th>{{Str::ucfirst(trans('app.actions'))}}</th>
+                            <th>@langTitle('app.image')</th>
+                            <th>@langTitle('app.title')</th>
+                            <th>@langTitle('app.date')</th>
+                            <th>@langTitle('app.description')</th>
+                            <th>@langTitle('app.runtime')</th>
+                            <th>@langTitle('app.rating')</th>
+                            <th>@langTitle('app.genres')</th>
+                            <th>@langTitle('app.actors')</th>
+                            <th>@langTitle('app.created_at')</th>
+                            <th>@langTitle('app.updated_at')</th>
+                            <th>@langTitle('app.actions')</th>
                         </tr>
                     </tfoot>
                 </table>
             </div>
-            <!-- /.card-body -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.col -->
 </div>
-<!-- /.row -->
 @endsection
