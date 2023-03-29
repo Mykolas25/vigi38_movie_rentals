79959c8e74511f586f20c20985848a288913a774
Mykolas25 <justmikis@gmail.com>
2023-03-29 14:28:57 +0300

    Update create.blade.php
    translate labels and placeholders
    add var title to layout document extend

--- resources/views/admin/actors/create.blade.php
+++ resources/views/admin/actors/create.blade.php
@@ -1,29 +1,29 @@
-@extends('admin.layouts.document')
+@extends('admin.layouts.document', ['title' => Str::title(__('app.actors'))])
 
 @section('content')
 <div class="card card-primary">
     <div class="card-header">
-        <h3 class="card-title">Naujas aktorius</h3>
+        <h3 class="card-title">@langTitle('app.new')</h3>
     </div>
     <form action="{{ route('admin.actors.store') }}" method="POST">
         @csrf
         <div class="card-body">
             <div class="form-group">
-                <label for="first-name">Vardas</label>
-                <input type="text" class="form-control" name="first_name" id="first-name" placeholder="Vardas">
+                <label for="first-name">@langTitle('app.first_name')</label>
+                <input type="text" class="form-control" name="first_name" id="first-name" placeholder="@lang('first_name')">
             </div>
 
             <div class="form-group">
-                <label for="last-name">Pavardė</label>
-                <input type="text" class="form-control" name="last_name" id="last-name" placeholder="Pavardė">
+                <label for="last-name">@langTitle('app.last_name')</label>
+                <input type="text" class="form-control" name="last_name" id="last-name" placeholder="@lang('last_name')">
             </div>
 
             <div class="form-group">
-                <label for="date-of-birth">Gimimo data</label>
-                <input type="date" class="form-control" name="date_of_birth" id="date-of-birth" placeholder="Gimimo data">
+                <label for="date-of-birth">@langTitle('app.date_of_birth')</label>
+                <input type="date" class="form-control" name="date_of_birth" id="date-of-birth" placeholder="@lang('date_of_birth')">
             </div>
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">@langTitle('app.submit')</button>
         </div>
     </form>
 </div>
