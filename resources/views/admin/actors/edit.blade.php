0b343ae2b36218b6f37d64b92192b695839d9788
Mykolas25 <justmikis@gmail.com>
2023-03-29 13:42:21 +0300

    Update edit.blade.php
    use custom @lang directives for translations.
    rename var $actor to var $model

--- resources/views/admin/actors/edit.blade.php
+++ resources/views/admin/actors/edit.blade.php
@@ -3,36 +3,36 @@
 @section('content')
     <div class="card card-primary">
         <div class="card-header">
-            <h3 class="card-title">Keisti aktorių [{{ ($actor->fullName ?? '')  }}][{{ $actor->id ?? '' }}]</h3>
+            <h3 class="card-title">@langTitle('app.edit') [{{ ($model->fullName ?? '')  }}][{{ $model->id ?? '' }}]</h3>
         </div>
-        <form action="{{ route('admin.actors.update', $actor) }}" method="POST">
+        <form action="{{ route('admin.actors.update', $model) }}" method="POST">
             @method('put')
             @csrf
-            <input type="hidden" name="id" value="{{ $actor->id ?? '' }}">
+            <input type="hidden" name="id" value="{{ $model->id ?? '' }}">
             <div class="card-body">
                 <div class="form-group">
                     <div class="form-group">
-                        <label for="first-name">Vardas</label>
+                        <label for="first-name">@langTitle('app.first_name')</label>
                         <input type="text" class="form-control" name="first_name" id="first-name"
-                            value="{{ $actor->first_name ?? '' }}" placeholder="Vardas">
+                            value="{{ $model->first_name ?? '' }}" placeholder="@lang('app.first_name')">
                     </div>
 
                     <div class="form-group">
-                        <label for="last-name">Pavardė</label>
+                        <label for="last-name">@langTitle('app.last_name')</label>
                         <input type="text" class="form-control" name="last_name" id="last-name"
-                            value="{{ $actor->last_name ?? '' }}" placeholder="Pavardė">
+                            value="{{ $model->last_name ?? '' }}" placeholder="@lang('app.last_name')">
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="date-of-birth">Data</label>
+                    <label for="date-of-birth">@langTitle('app.date_of_birth')</label>
                     <input type="date" class="form-control" name="date_of_birth"
-                        value="{{ $actor->date_of_birth ?? '' }}" id="date-of-birth" placeholder="Gimimo data">
+                        value="{{ $model->date_of_birth ?? '' }}" id="date-of-birth" placeholder="@lang('app.date_of_birth')">
                 </div>
             </div>
 
             <div class="card-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">@langTitle('app.submit')</button>
             </div>
         </form>
     </div>
