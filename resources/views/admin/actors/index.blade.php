61c22fa9fc33a42a3bda8b4230e4787a7b4b789a
Mykolas25 <justmikis@gmail.com>
2023-03-29 13:43:16 +0300

    Update index.blade.php
    use  custom @lang directives for translations

--- resources/views/admin/actors/index.blade.php
+++ resources/views/admin/actors/index.blade.php
@@ -1,74 +1,71 @@
 @extends('admin.layouts.document')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ Str::ucfirst(trans('app.actors')) }}</h3>
-                    <a href="{{ route('admin.actors.create') }} " class="btn btn-app">
-                        <i class="fas fa-plus"></i> {{ Str::ucfirst(trans('app.new')) }}
-                    </a>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <table id="admin-table" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>{{ Str::ucfirst(trans('app.first_Name')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.last_name')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.date_of_birth')) }} </th>
-                                <th>{{ Str::ucfirst(trans('app.created_at')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.updated_at')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.actions')) }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($models as $model)
-                                <tr>
-                                    <td>{{ $model->id ?? '' }} </td>
-                                    {{-- <td>
-                                    <img width="100" src="{{ ($model->image ?? '')}} ">
-                                </td> --}}
-                                    <td> {{ $model->first_name ?? '' }} </td>
-                                    <td> {{ $model->last_name ?? '' }} </td>
-                                    <td> {{ $model->date_of_birth ?? '' }} </td>
-                                    <td> {{ $model->created_at ?? '' }} </td>
-                                    <td> {{ $model->updated_at ?? '' }} </td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <a href='{{ route('admin.actors.edit', $model) }} ' type="button"
-                                                class="btn btn-info">Keisti</a>
-                                            <button type="button" class="btn btn-info dropdown-toggle dropdown-icon"
-                                                data-toggle="dropdown" aria-expanded="false">
-                                                <span class="sr-only">Toggle Dropdown</span>
-                                            </button>
-                                            <div class="dropdown-menu" role="menu">
-                                                <a onclick="event.preventDefault()" class="dropdown-item delete"
-                                                    href="{{ route('admin.actors.destroy', $model) }} ">Naikinti</a>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                            <th>ID</th>
-                                <th>{{ Str::ucfirst(trans('app.first_Name')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.last_name')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.date_of_birth')) }} </th>
-                                <th>{{ Str::ucfirst(trans('app.created_at')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.updated_at')) }}</th>
-                                <th>{{ Str::ucfirst(trans('app.actions')) }}</th>
-                        </tfoot>
-                    </table>
-                </div>
-                <!-- /.card-body -->
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">@langTitle('app.actors')</h3>
+                <a href="{{ route('admin.actors.create') }} " class="btn btn-app">
+                    <i class="fas fa-plus"></i> @langTitle('app.new')
+                </a>
             </div>
-            <!-- /.card -->
+            <div class="card-body">
+                <table id="admin-table" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>@langTitle('app.first_Name')</th>
+                            <th>@langTitle('app.last_name')</th>
+                            <th>@langTitle('app.date_of_birth') </th>
+                            <th>@langTitle('app.created_at')</th>
+                            <th>@langTitle('app.updated_at')</th>
+                            <th>@langTitle('app.actions')</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($models as $model)
+                        <tr>
+                            <td>{{ $model->id ?? '' }} </td>
+                            {{-- <td>
+                                <img width="100" src="{{ ($model->image ?? '')}} ">
+                            </td> --}}
+                            <td> {{ $model->first_name ?? '' }} </td>
+                            <td> {{ $model->last_name ?? '' }} </td>
+                            <td> {{ $model->date_of_birth ?? '' }} </td>
+                            <td> {{ $model->created_at ?? '' }} </td>
+                            <td> {{ $model->updated_at ?? '' }} </td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{ route('admin.actors.edit', $model) }}" type="button"
+                                        class="btn btn-info">@langTitle('app.edit')</a>
+                                    <button type="button" class="btn btn-info dropdown-toggle dropdown-icon"
+                                        data-toggle="dropdown" aria-expanded="false">
+                                        <span class="sr-only">Toggle Dropdown</span>
+                                    </button>
+                                    <div class="dropdown-menu" role="menu">
+                                        <a onclick="event.preventDefault()" class="dropdown-item delete"
+                                            href="{{ route('admin.actors.destroy', $model) }}">
+                                                @langTitle('app.delete')
+                                        </a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <th>ID</th>
+                        <th>@langTitle('app.first_Name')</th>
+                        <th>@langTitle('app.last_name')</th>
+                        <th>@langTitle('app.date_of_birth') </th>
+                        <th>@langTitle('app.created_at')</th>
+                        <th>@langTitle('app.updated_at')</th>
+                        <th>@langTitle('app.actions')</th>
+                    </tfoot>
+                </table>
+            </div>
         </div>
-        <!-- /.col -->
     </div>
-    <!-- /.row -->
+</div>
 @endsection
