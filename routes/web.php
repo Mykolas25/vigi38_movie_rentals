2d34a47c1ebce4d252aec40a966256c429d17f68
Mykolas25 <justmikis@gmail.com>
2023-03-29 22:16:01 +0300

    Update web.php
    add auth routes
    add comments
    add middleware

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Front\FrontHomeController;
-use App\Http\Controllers\Front\FrontMovieController;
 use App\Http\Controllers\Admin\AdminActorController;
 use App\Http\Controllers\Admin\AdminGenreController;
 use App\Http\Controllers\Admin\AdminMovieController;
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Front\FrontMovieController;
 use App\Http\Controllers\Admin\AdminCountryController;
 use App\Http\Controllers\Admin\AdminLanguageController;
+use App\Http\Controllers\Admin\Auth\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,22 +23,35 @@ use App\Http\Controllers\Admin\AdminLanguageController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [AdminHomeController::class, 'index'])->name('home');
-    Route::resources([
-        'movies' => AdminMovieController::class,
-        'actors' => AdminActorController::class,
-        'languages' => AdminLanguageController::class,
-        'countries' => AdminCountryController::class,
-        'genres' => AdminGenreController::class,
-    ]);
-});
-
+//Public routes, accesible to anyone
 Route::name('front.')->group(function () {
     Route::get('/', [FrontHomeController::class, 'index'])->name('home');
     Route::resource('movies', FrontMovieController::class)->only(['index', 'show']);
+});
+
+//Routes grouped under admin prefix - admin panel and admin auth routes
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    //Routes protected by auth middleware, accass to routes only for authenticated users
+    Route::middleware('auth')->group(function () {
+        Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+        Route::resources([
+            'movies' => AdminMovieController::class,
+            'actors' => AdminActorController::class,
+            'languages' => AdminLanguageController::class,
+            'countries' => AdminCountryController::class,
+            'genres' => AdminGenreController::class,
+        ]);
+        //Logout route accassible when authenticated
+        Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+    });
+
+    //Routes protected by guest middleware, accass to routes only for guest - not authenticated users
+    //If user authenticated, redirects to admin.home
+    Route::middleware('guest')->name('auth.')->group(function () {
+        Route::get('/login', [LoginController::class, 'login'])->name('login');
+        Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
+        Route::get('/registration', [RegistrationController::class, 'registration'])->name('registration');
+        Route::post('/register', [RegistrationController::class, 'register'])->name('register');
+    });
 });
