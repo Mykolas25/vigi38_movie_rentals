9ed703b9af24441b135a998c97ffdf6679b3dc52
Mykolas25 <justmikis@gmail.com>
2023-03-29 17:06:13 +0300

    Update create.blade.php
    translations

--- resources/views/admin/movies/create.blade.php
+++ resources/views/admin/movies/create.blade.php
@@ -1,38 +1,36 @@
-@extends('admin.layouts.document')
+@extends('admin.layouts.document', ['title' => Str::title(__('app.movies'))])
 
 @section('content')
 <div class="card card-primary">
     <div class="card-header">
-        <h3 class="card-title">Naujas filmas</h3>
+        <h3 class="card-title">@langTitle('app.new')</h3>
     </div>
-    <!-- /.card-header -->
-    <!-- form start -->
     <form action="{{ route('admin.movies.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="card-body">
             <div class="form-group">
-                <label for="title">Pavadinimas</label>
-                <input type="text" class="form-control" name="title" id="title" placeholder="Pavadinimas">
+                <label for="title">@langTitle('app.title')</label>
+                <input type="text" class="form-control" name="title" id="title" placeholder="@lang('app.title')">
             </div>
 
             <div class="form-group">
-                <label for="release-date">Data</label>
-                <input type="date" class="form-control" name="release_date" id="release-date" placeholder="Data">
+                <label for="release-date">@langTitle('app.release_date')</label>
+                <input type="date" class="form-control" name="release_date" id="release-date" placeholder="@lang('app.release_date')">
             </div>
 
             <div class="form-group">
-                <label for="description">Aprašymas</label>
-                <input type="text" class="form-control" name="description" id="description" placeholder="Aprašymas">
+                <label for="description">@langTitle('app.description')</label>
+                <input type="text" class="form-control" name="description" id="description" placeholder="@lang('app.description')">
             </div>
 
             <div class="form-group">
-                <label for="runtime">Trukmė</label>
-                <input type="number" class="form-control" max="1000" name="runtime" id="runtime" placeholder="Trukmė">
+                <label for="runtime">@langTitle('app.runtime')</label>
+                <input type="number" class="form-control" max="1000" name="runtime" id="runtime" placeholder="@lang('app.runtime')">
             </div>
 
             <div class="form-group">
-                <label for="rating">Reitingas</label>
-                <input type="text" class="form-control" maxlength="5" name="rating" id="rating" placeholder="Reitingas">
+                <label for="rating">@langTitle('app.rating')</label>
+                <input type="text" class="form-control" maxlength="5" name="rating" id="rating" placeholder="@lang('app.rating')">
             </div>
             <x-forms.multi-relation-select :tagName="'genres'" :relationItems="$genres" />
 
@@ -41,18 +39,18 @@
             <x-forms.multi-relation-select :tagName="'countries'" :relationItems="$countries" />
 
             <x-forms.multi-relation-select :tagName="'actors'" :relationItems="$actors" :optionDisplay="'fullName'" />
-            
+
             <div class="form-group">
                 <x-forms.image-input :label="'cover-image'" :inputName="'image'" :oldInputName="'old_cover_image'"/>
             </div>
 
-            <div class="form-group">    
+            <div class="form-group">
                 <x-forms.image-input :label="'images'" :inputName="'images[]'" :oldInputName="'old_images[]'"/>
             </div>
         </div>
-        
+
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">@langTitle('app.submit')</button>
         </div>
     </form>
 </div>
